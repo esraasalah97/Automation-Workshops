2f508cc2c4057cd8478193d103c1af742dba4a2d
Salah <esalah@deloitte.com>
2024-12-30 10:57:41 +0200

    upload project

---
+++ src/test/java/workshops/pagefactory/GooglePage.java
@@ -0,0 +1,6 @@
+package workshops.pagefactory;
+
+public class GooglePage {
+    //please add your name as a comment before your code
+
+}
---
+++ src/test/java/workshops/tests/GoogleTest.java
@@ -0,0 +1,11 @@
+package workshops.tests;
+
+public class GoogleTest {
+
+    //please add your name as a comment before your code
+
+
+
+
+
+}
